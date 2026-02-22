8d79fe33f983f85ab01b99e7d58c26b92c66281b
woreliedema <15200624002@163.com>
2026-02-22 14:22:29 +0800

    fix: python镜像版本修改

--- data_collection_service/app/main.py
+++ data_collection_service/app/main.py
@@ -83,5 +83,5 @@ app.include_router(
 
 if __name__ == "__main__":
     import uvicorn
-    # 本地调试启动命令，对应文档中的 uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+    # 本地调试启动命令，对应文档中的 uvicorn data_collection_service.app.main:app --reload --host 0.0.0.0 --port 8000
     uvicorn.run("data_collection_service.app.main:app", host="0.0.0.0", port=8000, reload=True)
